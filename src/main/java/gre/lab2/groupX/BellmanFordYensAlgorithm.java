b4ecbcd0fa0efef4b69ea1b755b6c4e17c9006c8
Jmuhleman <julien.muhlemann@heig-vd.ch>
2024-05-17 21:25:02 +0200

    first version of BFY

--- src/main/java/gre/lab2/groupX/BellmanFordYensAlgorithm.java
+++ src/main/java/gre/lab2/groupX/BellmanFordYensAlgorithm.java
@@ -4,9 +4,38 @@ import gre.lab2.graph.BFYResult;
 import gre.lab2.graph.IBellmanFordYensAlgorithm;
 import gre.lab2.graph.WeightedDigraph;
 
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
+
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
   @Override
   public BFYResult compute(WeightedDigraph graph, int from) {
-    return null;
+
+
+    int nVertices = graph.getNVertices();
+    int[] distances = new int[nVertices];
+    int[] predecessors = new int[nVertices];
+    Arrays.fill(distances, Integer.MAX_VALUE);
+    Arrays.fill(predecessors, BFYResult.UNREACHABLE);
+    distances[from] = 0;
+
+    // Relax edges |V| - 1 times
+    for (int i = 0; i < nVertices - 1; i++) {
+      for (int u = 0; u < nVertices; u++) {
+
+        for (WeightedDigraph.Edge edge : graph.getOutgoingEdges(u)) {
+          // If we find a better way
+          if (distances[u] != Integer.MAX_VALUE && distances[u] + edge.weight() < distances[edge.to()]) {
+            distances[edge.to()] = distances[u] + edge.weight();
+            predecessors[edge.to()] = u;
+          }
+        }
+      }
+    }
+
+    return new BFYResult.ShortestPathTree(distances, predecessors);
   }
+
+
 }
--- src/main/java/gre/lab2/groupX/Main.java
+++ src/main/java/gre/lab2/groupX/Main.java
@@ -1,9 +1,48 @@
 package gre.lab2.groupX;
-
+import gre.lab2.graph.WeightedDigraph;
+import gre.lab2.graph.WeightedDigraphReader;
+import gre.lab2.graph.BFYResult;
 import java.io.IOException;
+
 
 public final class Main {
   public static void main(String[] args) throws IOException {
+
+
+
+    //read the files
+    final String[] FILES = new String[] { "data/test.txt", "data/reseau1.txt", "data/reseau2.txt",
+                                          "data/reseau3.txt", "data/reseau4.txt"};
+
+    for (var file : FILES){
+        System.out.println("\n\n>> Calculating the shortest path of the graph in " + file + ":");
+        // Read the given graph file, parse it and return a DirectedGraph structure
+        WeightedDigraph graph = WeightedDigraphReader.fromFile(file);
+
+        BellmanFordYensAlgorithm bfy = new BellmanFordYensAlgorithm();
+        // Apply the BellmanFordYensAlgorithm algorithm from the vertex 0
+        BFYResult result = bfy.compute(graph, 0);
+
+         System.out.println("--");
+        /*
+        // Print the result
+        if (result instanceof BFYResult.ShortestPathTree) {
+            System.out.println("Shortest path tree:");
+            BFYResult.ShortestPathTree spt = (BFYResult.ShortestPathTree) result;
+            for (int i = 0; i < spt.distances().length; i++) {
+            System.out.println("Vertex " + i + ": " + spt.distances()[i] + " (predecessor: " + spt.predecessors()[i] + ")");
+            }
+        } else if (result instanceof BFYResult.NegativeCycle) {
+            System.out.println("Negative cycle found:");
+            BFYResult.NegativeCycle nc = (BFYResult.NegativeCycle) result;
+            //System.out.println("Cycle: " + nc.cycle());
+        }
+*/
+
+
+
+    }
+
     // TODO
     //  - Renommage du package ;
     //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
